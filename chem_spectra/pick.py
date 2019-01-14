45a882e1001ca07b77bf84de94efa0a1b350a798
jasonych99 <jasonych99@gmail.com>
2019-01-14 15:23:15 +0100

    unify api

--- chem_spectra/pick.py
+++ chem_spectra/pick.py
@@ -14,12 +14,12 @@ pk = Blueprint('pick', __name__)
 @pk.before_app_request
 def filter_remote_ip():
     trusted_servers = get_ip_white_list()
-    # if request.remote_addr not in trusted_servers:
-    #     abort(403)
+    if request.remote_addr not in trusted_servers:
+        abort(403)
 
 
-@pk.route('/zip_peak_jcamp_n_img', methods=['POST'])
-def zip_peak_jcamp_n_img():
+@pk.route('/zip_jcamp_n_img', methods=['POST'])
+def zip_jcamp_n_img():
     try:
         file = request.files['file']
         params = extract_params(request)
@@ -36,26 +36,8 @@ def zip_peak_jcamp_n_img():
         abort(500)
 
 
-@pk.route('/zip_edit_jcamp_n_img', methods=['POST'])
-def zip_edit_jcamp_n_img():
-    try:
-        file = request.files['file']
-        params = extract_params(request)
-        if file: # and allowed_file(file):
-            tf_jcamp, tf_img = convert2jcamp_img(file, params)
-            memory = to_zip_response([tf_jcamp, tf_img])
-            return send_file(
-                memory,
-                attachment_filename='spectrum.zip',
-                as_attachment=True
-            )
-        abort(400)
-    except:
-        abort(500)
-
-
-@pk.route('/zip_peak_in_jcamp', methods=['POST'])
-def zip_peak_in_jcamp():
+@pk.route('/zip_jcamp', methods=['POST'])
+def zip_jcamp():
     try:
         file = request.files['file']
         params = extract_params(request)
@@ -72,8 +54,8 @@ def zip_peak_in_jcamp():
         abort(500)
 
 
-@pk.route('/zip_peak_in_image', methods=['POST'])
-def zip_peak_in_image():
+@pk.route('/zip_image', methods=['POST'])
+def zip_image():
     try:
         file = request.files['file']
         params = extract_params(request)
@@ -90,25 +72,25 @@ def zip_peak_in_image():
         abort(500)
 
 
-@pk.route('/peak_in_jcamp', methods=['POST'])
-def peak_in_jcamp():
-    # try:
-    file = request.files['file']
-    params = extract_params(request)
-    if file: # and allowed_file(file):
-        tf_jcamp = convert2jcamp(file, params)
-        return send_file(
-            tf_jcamp,
-            attachment_filename='spectrum.jdx',
-            as_attachment=True
-        )
-    #     abort(400)
-    # except:
-    #     abort(500)
+@pk.route('/jcamp', methods=['POST'])
+def jcamp():
+    try:
+        file = request.files['file']
+        params = extract_params(request)
+        if file: # and allowed_file(file):
+            tf_jcamp = convert2jcamp(file, params)
+            return send_file(
+                tf_jcamp,
+                attachment_filename='spectrum.jdx',
+                as_attachment=True
+            )
+            abort(400)
+    except:
+        abort(500)
 
 
-@pk.route('/peak_in_image', methods=['POST'])
-def peak_in_image():
+@pk.route('/image', methods=['POST'])
+def image():
     try:
         file = request.files['file']
         params = extract_params(request)
--- tests/test_pick.py
+++ tests/test_pick.py
@@ -8,14 +8,14 @@ file_jdx = '13C-DEPT135.dx'
 peaks_str = '745.0957757310398,0.2787140606224312#1018.4864309069585,0.31625977127489585#1154.473492548866,0.32047998816450246'
 
 
-def test_zip_peak_jcamp_n_img(client):
+def test_zip_jcamp_n_img(client):
     with open(target_dir + source_dir + file_jdx, 'rb') as f:
         file_content = f.read()
     data = dict(
         file=(io.BytesIO(file_content), '13C-DEPT135.dx'),
     )
     response = client.post(
-        '/zip_peak_jcamp_n_img',
+        '/zip_jcamp_n_img',
         content_type='multipart/form-data',
         data=data
     )
@@ -24,7 +24,7 @@ def test_zip_peak_jcamp_n_img(client):
     assert response.mimetype == 'application/zip'
 
 
-def test_zip_edit_jcamp_n_img(client):
+def test_zip_jcamp_n_img_with_peaks_str(client):
     with open(target_dir + source_dir + file_jdx, 'rb') as f:
         file_content = f.read()
     data = dict(
@@ -32,7 +32,7 @@ def test_zip_edit_jcamp_n_img(client):
         peaks_str=peaks_str
     )
     response = client.post(
-        '/zip_edit_jcamp_n_img',
+        '/zip_jcamp_n_img',
         content_type='multipart/form-data',
         data=data
     )
@@ -41,14 +41,14 @@ def test_zip_edit_jcamp_n_img(client):
     assert response.mimetype == 'application/zip'
 
 
-def test_zip_peak_in_jcamp(client):
+def test_zip_jcamp(client):
     with open(target_dir + source_dir + file_jdx, 'rb') as f:
         file_content = f.read()
     data = dict(
         file=(io.BytesIO(file_content), '13C-DEPT135.dx'),
     )
     response = client.post(
-        '/zip_peak_in_jcamp',
+        '/zip_jcamp',
         content_type='multipart/form-data',
         data=data
     )
@@ -57,14 +57,14 @@ def test_zip_peak_in_jcamp(client):
     assert response.mimetype == 'application/zip'
 
 
-def test_zip_peak_in_image(client):
+def test_zip_image(client):
     with open(target_dir + source_dir + file_jdx, 'rb') as f:
         file_content = f.read()
     data = dict(
         file=(io.BytesIO(file_content), '13C-DEPT135.dx'),
     )
     response = client.post(
-        '/zip_peak_in_image',
+        '/zip_image',
         content_type='multipart/form-data',
         data=data
     )
@@ -73,14 +73,14 @@ def test_zip_peak_in_image(client):
     assert response.mimetype == 'application/zip'
 
 
-def test_peak_in_jcamp(client):
+def test_jcamp(client):
     with open(target_dir + source_dir + file_jdx, 'rb') as f:
         file_content = f.read()
     data = dict(
         file=(io.BytesIO(file_content), '13C-DEPT135.dx'),
     )
     response = client.post(
-        '/peak_in_jcamp',
+        '/jcamp',
         content_type='multipart/form-data',
         data=data
     )
@@ -89,14 +89,14 @@ def test_peak_in_jcamp(client):
     assert response.mimetype == 'application/octet-stream'
 
 
-def test_peak_in_image(client):
+def test_image(client):
     with open(target_dir + source_dir + file_jdx, 'rb') as f:
         file_content = f.read()
     data = dict(
         file=(io.BytesIO(file_content), '13C-DEPT135.dx'),
     )
     response = client.post(
-        '/peak_in_image',
+        '/image',
         content_type='multipart/form-data',
         data=data
     )
