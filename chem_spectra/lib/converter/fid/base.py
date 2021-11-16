e4060714e08b16c1f41ce88a10ffc5605f831002
Lan Le <lan.le@kit.edu>
2021-11-16 20:56:19 +0100

    fix fid parsing

--- chem_spectra/lib/converter/fid/base.py
+++ chem_spectra/lib/converter/fid/base.py
@@ -17,6 +17,7 @@ class FidBaseConverter:
         self.is_ir = self.__is_ir()
         self.is_tga = self.__is_tga()
         self.is_uv_vis = self.__is_uv_vis()
+        self.is_hplc_uv_vis = self.__is_hplc_uv_vis()
         self.is_xrd = self.__is_xrd()
         self.non_nmr = self.__non_nmr()
         self.ncl = self.__ncl()
@@ -68,7 +69,7 @@ class FidBaseConverter:
         return self.typ in ['INFRARED', 'RAMAN', 'UVVIS']
 
     def __non_nmr(self):
-        return self.typ in ['INFRARED', 'RAMAN', 'UVVIS', 'THERMOGRAVIMETRIC ANALYSIS', 'MS', 'X-RAY DIFFRACTION']
+        return self.typ in ['INFRARED', 'RAMAN', 'UVVIS', 'HPLC UVVIS', 'THERMOGRAVIMETRIC ANALYSIS', 'MS', 'X-RAY DIFFRACTION']
 
     def __is_ir(self):
         return self.typ in ['INFRARED']
@@ -78,6 +79,9 @@ class FidBaseConverter:
 
     def __is_uv_vis(self):
         return self.typ in ['UVVIS']
+
+    def __is_hplc_uv_vis(self):
+        return self.typ in ['HPLC UVVIS']
 
     def __is_xrd(self):
         return self.typ in ['X-RAY DIFFRACTION']
