44af2ddb31dcf718760e0ee392ecaeacf8165f43
Lan Le <lan.le@kit.edu>
2022-07-20 13:51:29 +0200

    update to version 0.10.14-beta.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='chem-spectra-app',
-    version='0.10.14-beta.4',
+    version='0.10.14-beta.5',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
