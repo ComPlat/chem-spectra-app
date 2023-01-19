416723dccf8f070e7f867b4828c925e28f18e52b
Lan Le <baolan2005@gmail.com>
2023-01-19 09:33:51 +0100

    fix cannot read processed data in Bruker FID (#74)
    Co-authored-by: Lan Le <lan.le@kit.edu>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='chem-spectra-app',
-    version='0.10.17-patch.1',
+    version='0.10.17-patch.2',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
