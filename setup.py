78e6fb758473edd9b7eccbeec1d699ebcf25959b
Lan Le <lan.le@kit.edu>
2023-09-26 13:44:56 +0200

    chore: change axes label for CV layout

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='chem-spectra-app',
-    version='0.13.4',
+    version='0.13.4-patch.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
