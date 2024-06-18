cb175fda2054d67343862b87ff50e9ba63ac6c67
Lan Le <lan.le@kit.edu>
2024-06-18 12:53:26 +0200

    feat: handle GC layout

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='chem-spectra-app',
-    version='1.2.2',
+    version='1.2.3',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
