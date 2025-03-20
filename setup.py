51aa1b9798fc2989c5976fddd8b1c96429b0f2db
Nicolass67 <99028040+Nicolass67@users.noreply.github.com>
2025-03-20 10:42:49 +0100

    chore: bump version tag from 1.2.4 to 1.2.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='chem-spectra-app',
-    version='1.2.4',
+    version='1.2.5',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
